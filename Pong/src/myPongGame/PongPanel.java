ad0066f1ef80ce4774d2c59ba57c2e9689fe0781
Otagotago <nicohendrikse@ymail.com>
2024-08-20 09:12:12 +1200

    Updated Pong game on page "Spite class" for OP

--- Pong/src/myPongGame/PongPanel.java
+++ Pong/src/myPongGame/PongPanel.java
@@ -1,7 +1,6 @@
 package myPongGame;
 
-import java.awt.Color;
-
+   import java.awt.Color;
    import java.awt.event.ActionEvent;
    import java.awt.event.ActionListener;
    import java.awt.event.KeyEvent;
@@ -10,6 +9,8 @@ import java.awt.Color;
    import javax.swing.Timer;
    import java.awt.Graphics;
    import java.awt.Graphics2D;
+   import java.awt.Stroke;
+   import java.awt.BasicStroke;
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	   
@@ -25,14 +26,23 @@ import java.awt.Color;
 	   		Timer timer = new Timer(TIMER_DELAY, this);
 
 	   			timer.start();
-
+	   	
 	    }
+	   
+	   private void paintDottedLine(Graphics g) {
+	   		Graphics2D g2d = (Graphics2D) g.create();
+	   			Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	   			g2d.setStroke(dashed);
+	   			g2d.setPaint(Color.WHITE);
+	   			g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	   			g2d.dispose();
+	   				 }
+	   
 	   
 	   @Override
 	  public void paintComponent(Graphics g) {
 	       super.paintComponent(g);
-	       g.setColor(Color.WHITE);
-	       g.fillRect(20, 20, 100, 100);
+	       paintDottedLine(g);
 	  }
 	   
       @Override
@@ -49,11 +59,8 @@ import java.awt.Color;
   
       @Override
       public void actionPerformed(ActionEvent event) {
+    	  update();
+    	  repaint();
       }
       
-      public void update(ActionEvent event) {
-      }
-      
-      public void repaint(ActionEvent event) {
-      }
    }
